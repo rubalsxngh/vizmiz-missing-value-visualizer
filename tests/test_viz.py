a09c6ce716d9a0594b81a34676e40e3cb7574e59
Rubal Singh <rubalsxngh@gmail.com>
2023-12-21 00:47:33 +0530

    add testcase for dendogram

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 import unittest
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class test_spec_visualizer(unittest.TestCase):
     def test_spec(self):
@@ -17,7 +21,7 @@ class test_spec_visualizer(unittest.TestCase):
         self.assertIsInstance(viz.vizspectrum(df), go.Figure)
 
 
-class test_bar_visulaizer(unittest.TestCase):
+class test_bar_visualizer(unittest.TestCase):
     def test_bar(self):
         df= pd.read_csv('D:/my_projects/vizmiz-missing-value-visualizer/data/baby_names.csv')
         '''
@@ -26,7 +30,7 @@ class test_bar_visulaizer(unittest.TestCase):
 
         self.assertIsInstance(viz.vizbar(df), go.Figure)
 
-class test_heatmap_visulaizer(unittest.TestCase):
+class test_heatmap_visualizer(unittest.TestCase):
     def test_heatmap(self):
         df= pd.read_csv('D:/my_projects/vizmiz-missing-value-visualizer/data/baby_names.csv')
         '''
@@ -34,3 +38,13 @@ class test_heatmap_visulaizer(unittest.TestCase):
         '''
 
         self.assertIsInstance(viz.heatmap(df), plt.Figure)
+
+
+class test_dendogram_visualizer(unittest.TestCase):
+    def test_dendogram(self):
+        df= pd.read_csv('D:/my_projects/vizmiz-missing-value-visualizer/data/baby_names.csv')
+        '''
+        this test use sample dataset from /data repo, can replace it with you own
+        '''
+
+        self.assertIsInstance(viz.vdendrogram(df), plt.Figure)
